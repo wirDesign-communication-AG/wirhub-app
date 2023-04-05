b3fb444b9f8837c10853a491aa8951da137573a6
Sven Joder <sj@pmdevelopment.de>
2023-04-05 17:16:00 +0200

    wip

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     AppBundle\AppBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
