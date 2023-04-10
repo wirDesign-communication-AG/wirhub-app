274e1095b69266d2bc5bc84ab0f765f9ad29d78f
Sven Joder <sj@pmdevelopment.de>
2023-04-10 12:52:17 +0200

    add missing bundles

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
