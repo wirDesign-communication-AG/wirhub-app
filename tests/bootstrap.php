06bd3ef92a04decd69ebdb06abe828f0667612e8
Sven Joder <sj@pmdevelopment.de>
2023-04-10 13:22:45 +0200

    load env in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+use Symfony\Component\Dotenv\Dotenv;
+
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
 ini_set('xdebug.max_nesting_level', 4000);
+
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
