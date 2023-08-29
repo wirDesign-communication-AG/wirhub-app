306d6d3afb69595b41f85f70a0cc4f3009c40631
Sven Joder <sj@pmdevelopment.de>
2023-08-29 10:38:42 +0200

    enforce ssl

--- public/index.php
+++ public/index.php
@@ -2,9 +2,11 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+$_SERVER['HTTPS'] = 'on';
 
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
 };
 
