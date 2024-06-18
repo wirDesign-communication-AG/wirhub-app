787fb950153fc65e336612b65a6a95707635049c
Sven Joder <sj@pmdevelopment.de>
2024-06-18 13:59:57 +0200

    enhanced config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -28,5 +28,11 @@ $finder = (new PhpCsFixer\Finder())
 return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
     ])
-    ->setFinder($finder);
+    ->setFinder($finder)
+    ->setLineEnding("\n");
