0422b625374b813265a7150cab01cd16b025d3cc
Sven Joder <sj@pmdevelopment.de>
2024-06-18 13:35:28 +0200

    Add PHP CS Fixer

--- public/index.php
+++ public/index.php
@@ -2,13 +2,12 @@
 
 use App\Kernel;
 
-require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     if ('prod' === $context['APP_ENV']) {
         $_SERVER['HTTPS'] = 'on';
     }
 
-    return new Kernel($context['APP_ENV'], (bool)$context['APP_DEBUG']);
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
-
