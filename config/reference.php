3ec01c9dd1a4bcb7e11420abf7bd2b37ee4656b0
Sven Joder <sj@pmdevelopment.de>
2026-09-11 14:35:23 +0200

    New beta

--- config/reference.php
+++ config/reference.php
@@ -1481,6 +1481,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *         filter_value_name?: scalar|Param|null, // Default: "filterValue"
  *         page_name?: scalar|Param|null, // Default: "page"
  *         distinct?: bool|Param, // Default: true
+ *         wrap_queries?: bool|Param, // Default: false
  *         page_out_of_range?: scalar|Param|null, // Default: "ignore"
  *         default_limit?: scalar|Param|null, // Default: 10
  *     },
